1e002a15064a41493c66be40906b03c30acaf3c6
Diana Carolina Rodríguez Ruiz <150412990+crrdiana@users.noreply.github.com>
2025-06-22 22:03:30 -0600

    Delete RETO 2/MarcaRepository.java

--- RETO 2/MarcaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ejemplo.tienda.repositorio;
-
-import com.ejemplo.tienda.modelo.Marca;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MarcaRepository extends JpaRepository<Marca, Long> {
-}
