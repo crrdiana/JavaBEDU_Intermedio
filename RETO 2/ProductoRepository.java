8de7da8e44f994f23212313e77065f2454273264
Diana Carolina Rodríguez Ruiz <150412990+crrdiana@users.noreply.github.com>
2025-06-22 22:03:45 -0600

    Delete RETO 2/ProductoRepository.java

--- RETO 2/ProductoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ejemplo.tienda.repositorio;
-
-import com.ejemplo.tienda.modelo.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
-}
